92a834facce68d28c65e2e9047913afeb64e743a
melissa-oliveira2005 <mcao2@aluno.ifal.edu.br>
2026-02-26 21:05:10 -0300

    feat: Criação de uma função auxiliar para responder JSON e implementação do GET/health

--- src/server.js
+++ src/server.js
@@ -5,15 +5,24 @@ import getRawBody from 'raw-body';
 //Variação 3 > recurso/requerimentos
 const hostname = '127.0.0.1';
 const PORT = 3000;
-const recurso = '/requerimentos';
+const recurso = '/health';
 
 let requerimentos = [];
 let nextId = 1;
+
+function sendJsonResponse(res, statusCode, data) {
+    res.statusCode = statusCode;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+}
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ message: 'Servidor rodando!' }));
+
+    if (req.method === 'GET' && req.url === '/health') {
+        return sendJsonResponse(res, 200, { status: 'ativo' });
 });
 
 server.listen(PORT, () => {
